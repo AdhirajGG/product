38395cc84188f4008426356dd5d7b36b785a4e20
AdhirajGG <adhiraj2003.27@gmail.com>
2025-04-18 18:36:48 +0530

    Fixed deployment configuration

--- Frontend/src/Context/AuthContext.jsx
+++ Frontend/src/Context/AuthContext.jsx
@@ -21,7 +21,7 @@ export const AuthProvider = ({ children }) => {
 
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
-      {children}
+         {children}
     </AuthContext.Provider>
   );
 };
