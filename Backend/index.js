ad3876faccf7b6a6fcd3fa769c9c9359eded00c6
AdhirajGG <adhiraj2003.27@gmail.com>
2025-04-18 17:42:35 +0530

    Ready for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Serve static files from the React frontend app
+const __dirname = path.resolve();
 
 // Middleware
 app.use(express.json());
@@ -20,6 +24,11 @@ import authRoutes from "./routes/auth.js";
 import productRoutes from "./routes/product.route.js";
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend","dist", "index.html"));
+});
 
 // Database connection
 connectDB();
